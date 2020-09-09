85e0b976385f3608097aa41800db841945a569e4
Viqui-Alaro <31290294+Viqui-Alaro@users.noreply.github.com>
2020-09-08 22:13:36 -0400

    Create 02-NúmerosString.dart

---
+++ 02-NúmerosString.dart
@@ -0,0 +1,21 @@
+
+//Tipos de datos
+void main() {
+
+  // Números
+  int empleados = 10;
+  double pi = 3.14159;
+  var numero= 1.0;
+  
+  print('$empleados - $pi - $numero');
+  
+  // String - cademas de caracteres
+  String nombre= 'Viqui';
+  
+  
+  print(nombre);
+  print(nombre[0]);
+  print(nombre[nombre.length -1]);
+  
+  
+}
